0eb0e1d662cdcea7b6eaa989d7e4da2f1f921b94
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-07-05 11:16:58 -0500

    Add AngularJS project structure

---
+++ src/public/public.module.js
@@ -0,0 +1,8 @@
+(function () {
+  'use strict';
+  /**
+   * Public GrupoAcera application. Includes the common module and ui-router.
+   */
+  angular.module('public', ['ui.router']);
+
+})();
---
+++ src/public/public.routes.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+  angular.module('public')
+  .config(routeConfig);
+
+  /**
+  * Configures the routes and the views
+  */
+  routeConfig.$inject = ['$stateProvider'];
+
+  function routeConfig($stateProvider) {
+    //Routes
+    $stateProvider
+    .state('public', {
+      abstract: true,
+      templateUrl:'src/public/public.tpl.html'
+    })
+    .state('public.home', {
+      url: '/',
+      templateUrl:'src/public/home/home.tpl.html'
+    });
+
+  }
+
+})();
